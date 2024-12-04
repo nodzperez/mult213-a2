891349e969a69eb999116b0bf66defcd123d7ac2
nodzperez <146012330+nodzperez@users.noreply.github.com>
2024-12-04 00:18:29 -0600

    Added a footer component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header";
+import Footer from "./Footer";
 
 export default function App() {
   return (
     <div>
       <Header title="Welcome to the Assignment Page!" message="Thanks for visiting this page!" />
+      <Footer message="Contact me at loverboy69@nodzperez.com" />
     </div>
   )
 
---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer({ message }) {
+    return (
+        <footer>
+            <p>{message}</p>
+            <p>Copyright © 2023 My Website. All rights reserved.</p>
+        </footer>
+    );
+}

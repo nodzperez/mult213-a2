28ea195510fa8a6607b750f2fab0334cbfc88ee5
nodzperez <146012330+nodzperez@users.noreply.github.com>
2024-12-04 23:01:27 -0600

    Added import for the footer.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,10 @@
+import React from "react";
+
 export default function Footer({ message }) {
-    return (
-        <footer>
-            <p>{message}</p>
-            <p>Copyright © 2023 My Website. All rights reserved.</p>
-        </footer>
-    );
+  return (
+    <footer>
+      <p>{message}</p>
+      <p>Copyright © 2023 My Website. All rights reserved.</p>
+    </footer>
+  );
 }
